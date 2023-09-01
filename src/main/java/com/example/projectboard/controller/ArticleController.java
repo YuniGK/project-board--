ef65cd94bf7b2421e3a8c50a0e65eb9fe0fb1219
yunkim <1106qwe@hanmail.net>
2023-09-01 19:46:40 +0900

    #6-게시글 상세 페이지 확인할 수 있도록 생성

--- src/main/java/com/example/projectboard/controller/ArticleController.java
+++ src/main/java/com/example/projectboard/controller/ArticleController.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -18,4 +19,12 @@ public class ArticleController {
         return "articles/index";
     }
 
+    @GetMapping("/{articleId}")
+    /* 요청 URI 매핑에서 템플릿 변수를 처리 하고 이를 메서드 매개변수로 설정할 수 있습니다. */
+    public String articles(@PathVariable long articleId, ModelMap map){
+        map.addAttribute("article", null);
+        map.addAttribute("articleComments", List.of());
+
+        return "articles/detail";
+    }
 }
