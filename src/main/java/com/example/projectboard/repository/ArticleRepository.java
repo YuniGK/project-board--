cd0501a689d48b2c70f5b95805f7816f3eea7bc1
yunkim <1106qwe@hanmail.net>
2023-09-01 14:58:51 +0900

    #1 - DataRest 설정
    게시글, 댓글의 json api를 자동으로 restful하게 만들어 질 수 있도록 설정

--- src/main/java/com/example/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/example/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.example.projectboard.repository;
 
 import com.example.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource//repository(저장소) 선언에 대한 어노테이션 - 내부적으로 Rest API가 만들어진다.
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
