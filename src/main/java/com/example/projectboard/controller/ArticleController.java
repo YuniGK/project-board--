88f4a405bc8f0db1fdbcf117126b1c6b7a1ce36b
yunkim <1106qwe@hanmail.net>
2023-09-01 19:49:26 +0900

    #6-게시글 컨트롤러 수정
    게시글 테스트를 위해 내용 추가 및 수정

--- src/main/java/com/example/projectboard/controller/ArticleController.java
+++ src/main/java/com/example/projectboard/controller/ArticleController.java
@@ -22,7 +22,7 @@ public class ArticleController {
     @GetMapping("/{articleId}")
     /* 요청 URI 매핑에서 템플릿 변수를 처리 하고 이를 메서드 매개변수로 설정할 수 있습니다. */
     public String articles(@PathVariable long articleId, ModelMap map){
-        map.addAttribute("article", null);
+        map.addAttribute("article", "article");//TODO: 구현시 실제 데이터를 넣어줘야한다.
         map.addAttribute("articleComments", List.of());
 
         return "articles/detail";
